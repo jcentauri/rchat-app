33075e24fc9ff1136fa5f8fc53aedc3255da3efe
jcentauri <59785979+jcentauri@users.noreply.github.com>
2020-01-12 17:12:06 +0800

    Update application.py

--- application.py
+++ application.py
@@ -10,10 +10,10 @@ from models import *
 # Configure app
 app = Flask(__name__)
 app.secret_key=os.environ.get('SECRET')
-app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 
 # Configure database
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
